373638091d2174a3ae9dfc95c327cda6d7babb62
Farda HOUMADI <farda.houmadi@hotmail.fr>
2019-02-12 15:30:45 +0100

    Profiles dev et prod

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,5 @@
 package dev;
-
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -11,10 +11,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-		String test = "a";
-		System.out.println(test);
 		
-		String asciiArt = FigletFont.convertOneLine("App");
-	    System.out.println(asciiArt);
+		String titre = ResourceBundle.getBundle("application").getString("titre");
+		String asciiArt = FigletFont.convertOneLine(titre);
+		System.out.println(asciiArt);
+		
+		// affichage de l'environnement
+		String environnement = ResourceBundle.getBundle("application").getString(
+		"environnement");
+		System.out.println("Environnement : " + environnement);
     }
 }
