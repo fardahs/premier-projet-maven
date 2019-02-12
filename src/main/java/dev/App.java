23b159efa2931a6d3779f8051eec0f9b8acb8ab6
Farda HOUMADI <farda.houmadi@hotmail.fr>
2019-02-12 11:45:21 +0100

    Intégration librairie tierce

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
@@ -9,7 +11,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-		String erreur = "a";
-		System.out.println(erreur);
+		String test = "a";
+		System.out.println(test);
+		
+		String asciiArt = FigletFont.convertOneLine("App");
+	    System.out.println(asciiArt);
     }
 }
